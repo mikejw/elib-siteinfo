66a02d032c68acae0daf3b60742ca88fbbde820d
Mike Whiting <mail@mikejw.co.uk>
2013-02-02 15:35:19 +0000

    working on new module.

--- src/Empathy/ELib/Settings/Controller.php
+++ src/Empathy/ELib/Settings/Controller.php
@@ -1,8 +1,7 @@
 <?php
 
-namespace Empathy\ELib\SiteInfo;
-
-use Empathy\ELib\AdminController,
+namespace Empathy\ELib\Settings;
+use Empathy\ELib\AdminController;
 
 
 
@@ -10,6 +9,14 @@ class Controller extends AdminController
 {
     public function default_event()
     {
-        
+        $settings_o = new \stdClass();
+
+        $settings_o->title = TITLE;
+        $settings_o->keywords = "mike michael whiting web developer programmer php lamp mysql javascript css perl linux";
+
+        $settings_o->description = "Michael Whiting is a web programmer from West Sussex, England.  With ten years experience of the web, some of his work has powered the Bestival website and also adorned eBay UK.";
+      
+        $this->assign('settings', $settings_o);
+        $this->setTemplate('elib:/admin/siteinfo/settings.tpl');
     }
 }
