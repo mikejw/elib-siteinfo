c6addd859061452cb6c348b10e4214f53b23463c
Mike Whiting <mail@mikejw.co.uk>
2023-10-12 22:11:12 +0100

    vendor support

--- src/Empathy/ELib/Settings/Controller.php
+++ src/Empathy/ELib/Settings/Controller.php
@@ -3,6 +3,7 @@
 namespace Empathy\ELib\Settings;
 
 use Empathy\ELib\AdminController;
+use Empathy\MVC\RequestException;
 use Empathy\MVC\VCache;
 use Empathy\MVC\DI;
 
@@ -10,6 +11,18 @@ use Empathy\MVC\DI;
 
 class Controller extends AdminController
 {
+
+
+    public function __construct($boot)
+    {
+        parent::__construct($boot);
+        $vendor = $this->stash->get('vendor');
+        if ($vendor) {
+            $this->stash->store('vendorId', $vendor['id']);
+        }
+    }
+
+
     public function clear_cache()
     {
         $cache = $this->stash->get('cache');
@@ -30,20 +43,39 @@ class Controller extends AdminController
 
     public function default_event()
     {
+        $vendorId = $this->stash->get('vendorId');
         $service = DI::getContainer()->get('SiteInfo');
         $settings = $service->getSettings();
 
         if(isset($_POST['cancel'])) {
             $this->redirect('admin');
         } elseif(isset($_POST['save'])) {
+            if ($vendorId) {
+                $vendorBean = \R::load('vendor', $vendorId);
+            }
+
             foreach($settings as $s) {
-                if($setting = \R::findOne('setting', 'name = ?', array($s))) {
+                $sql = 'name = ?';
+                $queryParams = array($s);
+                if ($vendorBean->id) {
+                    $sql .= ' and vendor_id = ?';
+                    array_push($queryParams, $vendorId);
+                } else {
+                    $sql .= ' and vendor_id is null';
+                }
+                if($setting = \R::findOne('setting', $sql, $queryParams)) {
                     $setting->value = $_POST[$s];
+                    if ($vendorBean->id) {
+                        $setting->vendor = $vendorBean;
+                    }
                     \R::store($setting);
                 } else {
                     $setting = \R::dispense('setting');
                     $setting->name = $s;
                     $setting->value = $_POST[$s];
+                    if ($vendorBean->id) {
+                        $setting->vendor = $vendorBean;
+                    }
                     \R::store($setting);
                 }
             }
--- src/Empathy/ELib/Settings/SiteInfo.php
+++ src/Empathy/ELib/Settings/SiteInfo.php
@@ -2,21 +2,35 @@
 
 namespace Empathy\ELib\Settings;
 
-
 use Empathy\MVC\DI;
 
 class SiteInfo
 {
+    private $vendorId = 0;
+
     public function __construct()
     {
         $this->settings = DI::getContainer()->get('SiteInfoSettings');
+        $vendor = DI::getContainer()->get('Stash')->get('vendor');
+        if ($vendor) {
+            $this->vendorId = $vendor['id'];
+        }
     }
 
     public function loadSettings()
     {
         $settings_o = new \stdClass();
         foreach($this->settings as $s) {
-            if($setting = \R::findOne('setting', 'name = ?', array($s))) {
+            $queryParams = array();
+            array_push($queryParams, $s);
+            $sql = 'name = ?';
+            if ($this->vendorId) {
+                $sql .= ' and vendor_id = ?';
+                array_push($queryParams, $this->vendorId);
+            } else {
+                $sql .= ' and vendor_id is null';
+            }
+            if($setting = \R::findOne('setting', $sql, $queryParams)) {
                 $settings_o->$s = $setting->value;
             }
         }
