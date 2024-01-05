76b0be0171f832dcb0c8c051d943169052c0f113
Mike Whiting <mail@mikejw.co.uk>
2024-01-05 17:25:14 +0000

    init cache

--- src/Empathy/ELib/MVC/Plugin/SiteInfo.php
+++ src/Empathy/ELib/MVC/Plugin/SiteInfo.php
@@ -11,6 +11,13 @@ class SiteInfo extends Plugin implements PreEvent
 
     public function onPreEvent()
     {
+        $cacheHost = str_replace('db-', 'cache-', Config::get('DB_SERVER'));
+        if ($cacheHost === "") {
+            $cacheHost = 'cache';
+        }
+        $cache = new VCache($cacheHost, 11211, null, DI::getContainer()->get('CacheEnabled'));
+        $this->stash->store('cache', $cache);
+
         $controller = $this->bootstrap->getController();
         $stash = DI::getContainer()->get('Stash');
         $siteInfoService = DI::getContainer()->get('SiteInfo');
