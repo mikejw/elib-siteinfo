9eb53edab93c1c1b5d6625d377e94ccb30c43c95
Mike Whiting <mail@mikejw.co.uk>
2024-01-05 17:47:20 +0000

    small fix

--- src/Empathy/ELib/MVC/Plugin/SiteInfo.php
+++ src/Empathy/ELib/MVC/Plugin/SiteInfo.php
@@ -13,17 +13,16 @@ class SiteInfo extends Plugin implements PreEvent
 
     public function onPreEvent()
     {
+        $stash = DI::getContainer()->get('Stash');
         $cacheHost = str_replace('db-', 'cache-', Config::get('DB_SERVER'));
         if ($cacheHost === "") {
             $cacheHost = 'cache';
         }
         $cache = new VCache($cacheHost, 11211, null, DI::getContainer()->get('CacheEnabled'));
-        $this->stash->store('cache', $cache);
+        $stash->store('cache', $cache);
 
         $controller = $this->bootstrap->getController();
-        $stash = DI::getContainer()->get('Stash');
         $siteInfoService = DI::getContainer()->get('SiteInfo');
-        $cache = DI::getContainer()->get('Stash')->get('cache');
         $info = $cache->cachedCallback('site_info', array($siteInfoService, 'getAll'));
         $controller->assign('site_info', $info);
         $stash->store('site_info', $info);
