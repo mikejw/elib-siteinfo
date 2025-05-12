43f49f7f8f2bbfd62db80cb6a739498303390d0e
Mike Whiting <mikejw3@gmail.com>
2025-05-12 14:48:56 +0100

    di fix

--- src/Empathy/ELib/MVC/Plugin/SiteInfo.php
+++ src/Empathy/ELib/MVC/Plugin/SiteInfo.php
@@ -19,16 +19,16 @@ class SiteInfo extends Plugin implements PreDispatch, PreEvent
     public function onPreDispatch()
     {
         $stash = DI::getContainer()->get('Stash');
-	$cache = DI::getContainer()->get('Cache');
-        $stash->store('cache', $cache);;
-	
+	    $cache = DI::getContainer()->get('Cache');
+        $stash->store('cache', $cache);
+        
         $siteInfoService = DI::getContainer()->get('SiteInfo');
         $stash->store('site_info', $cache->cachedCallback('site_info', array($siteInfoService, 'getAll')));
     }
     
     public function onPreEvent()
     {
-        $controller = $this->bootstrap->getController();
+        $controller = DI::getContainer()->get('Controller');
         $controller->assign('site_info', DI::getContainer()->get('Stash')->get('site_info'));
     }
 }
