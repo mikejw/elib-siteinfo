e7889d9aaa650db240e361d4328419994e7a83c1
Mike Whiting <mail@mikejw.co.uk>
2026-03-22 04:32:03 +0000

    add more types

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -2,11 +2,12 @@
 # Changelog
 
 
-## [4.1.3] - 2026-03-21
+## [4.1.3] - 2026-03-22
 
 ### Fixed
 
 - Fix method signatures
+- Fix module gen types
 
 
 ## [4.1.2] - 2026-03-03
--- src/Empathy/ELib/Gen/AdminSettings.php
+++ src/Empathy/ELib/Gen/AdminSettings.php
@@ -4,8 +4,8 @@ namespace Empathy\ELib\Gen;
 
 class AdminSettings extends \Empathy\MVC\Util\ControllerGen
 {
-    protected $name = 'settings';
-    protected $module = 'admin';
-    protected $parent = '\Empathy\ELib\Settings\Controller';
+    protected string $name = 'settings';
+    protected string $module = 'admin';
+    protected string $parent = '\Empathy\ELib\Settings\Controller';
 }
 
